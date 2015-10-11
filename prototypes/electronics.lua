74c701db8479c78c3ce9bd956361cd7265c0a9c1
Ranakastrasz <Master_of_Ra@verizon.net>
2015-10-11 16:07:25 -0400

    Added productivity module compatibility, Fixed Blast furnace tech icon.

--- prototypes/electronics.lua
+++ prototypes/electronics.lua
@@ -455,13 +455,13 @@ data:extend({
  
 })
 
-for _, assembler in pairs (data.raw["assembling-machine"]) do
+--[[for _, assembler in pairs (data.raw["assembling-machine"]) do
     for _, category in pairs (assembler.crafting_categories) do
         if category == "crafting" then
             table.insert(assembler.crafting_categories,"electronics")
         end
     end
-end
+end]]--
 
 data.raw["item"]["electronic-circuit"].subgroup = "cartmen-circuit"
 data.raw["item"]["advanced-circuit"].subgroup = "cartmen-circuit"
--- prototypes/overides.lua
+++ prototypes/overides.lua
@@ -5,3 +5,50 @@ for _, assembler in pairs (data.raw["assembling-machine"]) do
         end
     end
 end
+
+for km, vm in pairs(data.raw.module) do
+  if vm.name:find("productivity%-module") and vm.limitation then
+      table.insert(vm.limitation, "steel-plate-2")
+      table.insert(vm.limitation, "iron-plate-2")
+      table.insert(vm.limitation, "electronic-circuit-2")
+      table.insert(vm.limitation, "electronic-circuit-3")
+      table.insert(vm.limitation, "electronic-circuit-4")
+      
+      table.insert(vm.limitation, "wcb")
+      table.insert(vm.limitation, "prepped-wcb")
+      table.insert(vm.limitation, "pcb")
+      table.insert(vm.limitation, "prepped-pcb")
+      
+      table.insert(vm.limitation, "copper-sheet")
+      table.insert(vm.limitation, "silicon-sheet")
+      
+      table.insert(vm.limitation, "electronic-components-1")
+      table.insert(vm.limitation, "electronic-components-2")
+      table.insert(vm.limitation, "mini-electronic-components-1")
+      
+      table.insert(vm.limitation, "single-pcb")
+      table.insert(vm.limitation, "double-pcb")
+      table.insert(vm.limitation, "triple-pcb")
+      table.insert(vm.limitation, "quadruple-pcb")
+      
+      table.insert(vm.limitation, "integrated-circuit-1")
+      table.insert(vm.limitation, "integrated-circuit-2")
+      
+      table.insert(vm.limitation, "advanced-circuit")
+      table.insert(vm.limitation, "advanced-circuit-2")
+      table.insert(vm.limitation, "advanced-circuit-3")
+      table.insert(vm.limitation, "advanced-circuit-4")
+      table.insert(vm.limitation, "advanced-circuit-5")
+      
+      table.insert(vm.limitation, "processing-core-1-1")
+      table.insert(vm.limitation, "processing-core-1-2")
+      table.insert(vm.limitation, "processing-core-1-3")
+      
+      table.insert(vm.limitation, "processing-unit")
+      table.insert(vm.limitation, "processing-unit-2")
+      table.insert(vm.limitation, "processing-unit-3")
+      
+      
+      
+  end
+end
